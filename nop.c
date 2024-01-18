ab055964b90fd026db2aea6c4665d523cad5c1d6
TamirKhalid <tamir2khalid@gmail.com>
2024-01-17 16:33:52 -0800

    file

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - function
+ * @stack: main stack
+ * @line_cnt : noth
+ * Return: N\A
+ */
+
+void nop(stack_t **stack, unsigned int line_cnt)
+{
+(void) stack;
+(void) line_cnt;
+}
